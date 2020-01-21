cb8a365bec5c80bf954e4b43abb61300608d874a
Qizhen (Justsin) Ding <justin.qizhen.ding@gmail.com>
2020-01-21 11:58:40 -0500

    Add files via upload

--- NJ_prject/code/template_data_2012.py
+++ NJ_prject/code/template_data_2012.py
@@ -1,8 +1,39 @@
 import pandas as pd 
 
 
+# df = pd.read_csv('https://raw.githubusercontent.com/sai-cs-stud/NJLegisAnalysis/master/NJ_prject/Pandas/County_municipality.csv')
+# df_crime = pd.read_csv('https://raw.githubusercontent.com/sai-cs-stud/NJLegisAnalysis/master/NJ_prject/Pandas/NJ_Crime_2012.csv')
+# df_crime['County_Municipality'] = df_crime['County']+' '+df_crime['Municipality']
+# df_crime = df_crime.replace(to_replace ="Township",value ="Twp")
+# df_crime = df_crime.replace(to_replace ="-",value ="0")
+
+# county_to_municipality = {}
+# county_list= df.keys().tolist()
+
+
+# for county_name in county_list:
+#     df_clean = df.dropna(how='any', subset=[county_name])
+#     df_clean = df_clean.replace(to_replace ="Township",value ="Twp")
+#     municipality_name = df_clean[county_name].tolist()
+#     df_curr=(df_crime[df_crime['County'] == county_name]).set_index('Municipality')
+#     municipality_value = {i : df_curr.loc[i,'Violent Crimes'] for i in municipality_name}
+#     county_to_municipality[county_name] = municipality_value
+# county_to_municipality.items()
+# #print(county_to_municipality)   
+
+# summary={}
+# for county_name in county_to_municipality.keys():
+#     new_list = 0
+#     for values in county_to_municipality[county_name].values():
+#         if '.' in values:
+#             new_list+=round(float(values),2)
+#         else:
+#             new_list+= int(values)
+#     summary.update({county_name:new_list})
+# print(summary.items())
+
 df = pd.read_csv('https://raw.githubusercontent.com/sai-cs-stud/NJLegisAnalysis/master/NJ_prject/Pandas/County_municipality.csv')
-df_crime = pd.read_csv('https://raw.githubusercontent.com/sai-cs-stud/NJLegisAnalysis/master/NJ_prject/Pandas/NJ_Crime_2012.csv')
+df_crime = pd.read_csv('https://raw.githubusercontent.com/sai-cs-stud/NJLegisAnalysis/master/2012_data/NJ_Voters%26turnout_2012_clean.csv')
 df_crime['County_Municipality'] = df_crime['County']+' '+df_crime['Municipality']
 df_crime = df_crime.replace(to_replace ="Township",value ="Twp")
 df_crime = df_crime.replace(to_replace ="-",value ="0")
@@ -16,7 +47,7 @@ for county_name in county_list:
     df_clean = df_clean.replace(to_replace ="Township",value ="Twp")
     municipality_name = df_clean[county_name].tolist()
     df_curr=(df_crime[df_crime['County'] == county_name]).set_index('Municipality')
-    municipality_value = {i : df_curr.loc[i,'Violent Crimes'] for i in municipality_name}
+    municipality_value = {i : df_curr.loc[i,'% Republicans'] for i in municipality_name}
     county_to_municipality[county_name] = municipality_value
 county_to_municipality.items()
 #print(county_to_municipality)   
@@ -25,6 +56,6 @@ summary={}
 for county_name in county_to_municipality.keys():
     new_list = 0
     for values in county_to_municipality[county_name].values():
-        new_list+=int(values)
-    summary.update({county_name:new_list})
+        new_list += float(values)
+    summary.update({county_name:"%.2f" % new_list})
 print(summary.items())
